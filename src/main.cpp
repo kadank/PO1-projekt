d6b52feb2f054dbdcf6eef272a93d9241cd62941
kadank <kadank3@gmail.com>
2026-06-09 15:13:00 +0200

    dodano about

--- src/main.cpp
+++ src/main.cpp
@@ -83,11 +83,84 @@ public:
         SetMenuBar(menuBar);
 
         menuPanel = new wxPanel(this);
+        aboutPanel = new wxPanel(this);
         wxBoxSizer *menuOuterSizer = new wxBoxSizer(wxVERTICAL);
         wxBoxSizer *menuSizer = new wxBoxSizer(wxVERTICAL);
         wxStaticText *title = new wxStaticText(menuPanel, wxID_ANY, "BomberMan");
         title->SetFont(GetFont().Scale(3.0));
         menuSizer->Add(title, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+
+        //about kurwa--------------------------------------------------------------------------------------------------------
+
+        wxBoxSizer *aboutOuterSizer = new wxBoxSizer(wxVERTICAL);
+        wxBoxSizer *aboutSizer = new wxBoxSizer(wxVERTICAL);
+        wxStaticText *about = new wxStaticText(aboutPanel, wxID_ANY, "O autorach");
+        about->SetFont(GetFont().Scale(3.0));
+        aboutSizer->Add(about, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+
+        aboutOuterSizer->AddStretchSpacer(1);
+        aboutOuterSizer->Add(aboutSizer, 0, wxALIGN_CENTER);
+        aboutOuterSizer->AddStretchSpacer(1);
+        aboutPanel->SetSizer(aboutOuterSizer);
+
+        wxGridSizer *aboutContentSizer = new wxGridSizer (2,2,20,20);
+        aboutSizer->Add(aboutContentSizer, 0, wxALIGN_CENTER);
+
+
+        wxBoxSizer *Driansizer = new wxBoxSizer(wxVERTICAL);
+        Driansizer -> SetMinSize(160,100);
+        aboutContentSizer->Add(Driansizer, 0, wxALIGN_CENTER);
+
+        wxBoxSizer *Kejbasizer = new wxBoxSizer(wxVERTICAL);
+        Kejbasizer -> SetMinSize(160,100);
+        aboutContentSizer->Add(Kejbasizer, 0, wxALIGN_CENTER);
+
+        wxBoxSizer *Igorsizer = new wxBoxSizer(wxVERTICAL);
+        Igorsizer -> SetMinSize(160,100);
+        aboutContentSizer->Add(Igorsizer, 0, wxALIGN_CENTER);
+
+        wxBoxSizer *Kykolsizer = new wxBoxSizer(wxVERTICAL);
+        Kykolsizer -> SetMinSize(160,100);
+        aboutContentSizer->Add(Kykolsizer, 0, wxALIGN_CENTER);
+
+        wxStaticText *Drian = new wxStaticText(aboutPanel, wxID_ANY, "Drian");
+        wxStaticText *Driantext = new wxStaticText(aboutPanel, wxID_ANY, "No jestem gejem yyy monster hunter");
+        Drian->SetFont(GetFont().Scale(2.0));
+        Driansizer->Add(Drian, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+        Driansizer->Add(Driantext, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+
+
+        wxStaticText *Kejbas = new wxStaticText(aboutPanel, wxID_ANY, "Kejbas");
+        wxStaticText *Kejbastext = new wxStaticText(aboutPanel, wxID_ANY, "No jestem gejem yyy grzybie jebany");
+        Kejbas->SetFont(GetFont().Scale(2.0));
+        Kejbasizer->Add(Kejbas, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+        Kejbasizer->Add(Kejbastext, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+
+        wxStaticText *Igor = new wxStaticText(aboutPanel, wxID_ANY, "Igor");
+        wxStaticText *Igortext = new wxStaticText(aboutPanel, wxID_ANY, "No jestem heteroseksualista yyy world of goo");
+        Igor->SetFont(GetFont().Scale(2.0));
+        Igorsizer->Add(Igor, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+        Igorsizer->Add(Igortext, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+
+        wxStaticText *Kykol = new wxStaticText(aboutPanel, wxID_ANY, "Kykol");
+        wxStaticText *Kykoltext = new wxStaticText(aboutPanel, wxID_ANY, "No jestem gejem yyy i pedalem tez");
+        Kykol->SetFont(GetFont().Scale(2.0));
+        Kykolsizer->Add(Kykol, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+        Kykolsizer->Add(Kykoltext, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+
+        wxButton *aboutReturnButton = new wxButton(
+        aboutPanel, // parent (the panel!)
+            wxID_ANY, // ID
+            "Powrot", // label
+            wxPoint(10, 10), // position
+            wxSize(100, 30) // size
+        );
+
+        aboutReturnButton->Bind(wxEVT_BUTTON, &BombermanFrame::OnAboutReturn, this);
+        aboutSizer->Add(aboutReturnButton, 0, wxALIGN_CENTER | wxBOTTOM, 10);
+
+        //about koniec kurwa-------------------------------------------------------------------------------------------------
+
         wxButton *newGameButton = new wxButton(
             menuPanel, // parent (the panel!)
             wxID_ANY, // ID
@@ -113,6 +186,7 @@ public:
             wxPoint(10, 10), // position
             wxSize(100, 30) // size
         );
+
         quitButton->Bind(wxEVT_BUTTON, &BombermanFrame::OnExit, this);
         menuSizer->Add(quitButton, 0, wxEXPAND);
         menuOuterSizer->AddStretchSpacer(1);
@@ -126,10 +200,12 @@ public:
 
         sizer->Add(menuPanel, 1, wxEXPAND);
         sizer->Add(gamePanel, 1, wxEXPAND);
+        sizer->Add(aboutPanel,1,wxEXPAND);
 
         SetSizer(sizer);
 
         gamePanel->Hide();
+        aboutPanel -> Hide();
         gamePanel->Bind(wxEVT_KEY_DOWN, &BombermanFrame::OnKeyDown, this);
         gamePanel->Bind(wxEVT_KEY_UP, &BombermanFrame::OnKeyUp, this);
 
@@ -137,6 +213,7 @@ public:
 
 private:
     wxPanel* menuPanel;
+    wxPanel* aboutPanel;
     BombermanGame* gamePanel;
     Board board;
 
@@ -145,8 +222,10 @@ private:
         Close();
     }
     void OnAbout(wxCommandEvent& event) {
-        wxMessageBox("This is a wxWidgets Hello World example",
-             "About Hello World", wxOK | wxICON_INFORMATION);
+        menuPanel -> Hide();
+        gamePanel -> Hide();
+        aboutPanel -> Show();
+        //wxMessageBox("Autorzy \n Jakub Sakra \n Igor Lachowski \n Adrian Kisielewski \n Kacper 'Kykol' Kotulski", "About Us", wxOK | wxICON_INFORMATION);
     }
     void OnNewGame(wxCommandEvent &event) {
         menuPanel->Hide();
@@ -157,6 +236,11 @@ private:
     }
     void OnToggleFullscreen(wxCommandEvent &event) {
         this->ShowFullScreen(!this->IsFullScreen());
+    }
+
+    void OnAboutReturn(wxCommandEvent &event) {
+        menuPanel->Show();
+        aboutPanel->Hide();
     }
 
     void OnKeyDown(wxKeyEvent& event) {
